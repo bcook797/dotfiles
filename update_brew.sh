117904fcfea2945c34909c1520f4a6eafaee186a
Brandon Cook <bcook@thoughtworks.com>
2015-04-10 18:18:34 -0400

    Updated the update_brew.sh to included the path to brew

--- update_brew.sh
+++ update_brew.sh
@@ -1,1 +1,2 @@
+export PATH=/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 brew update && brew upgrade && brew tap homebrew/versions && brew tap caskroom/versions && brew cleanup && brew cask cleanup
