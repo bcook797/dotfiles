c99c98244ab28d5bd35030fc36185f7334dfed4f
Brandon Cook <bcook@thoughtworks.com>
2019-01-10 10:35:36 -0500

    Update setup script for oh-my-zsh configuration

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ git config --global credential.helper osxkeychain
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp bullet-train.zsh-theme ~/.oh-my-zsh/themes/bullet-train.zsh-theme
+cp ~/.zshrc ~/.zshrc.orig
+cp .zshrc ~/.zshrc
