03df9af9b0e618910712cac1a0cf298b25912b3a
Brandon Cook <bcook@thoughtworks.com>
2020-03-13 19:20:50 -0400

    Add git config aliases

--- setup.sh
+++ setup.sh
@@ -19,3 +19,8 @@ cp .zshrc ~/.zshrc
 # Install oh-my-zsh custom plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Create Git Aliases
+git config --global alias.pr pull -r
+git config --global alias.ps push
+git config --global alias.st status
